f651d03d91658c8dfb9fa5cad11c0158fd18d52a
k2room <kimyirum@gm.gist.ac.kr>
2024-05-16 11:33:15 +0900

    [mod] develop

--- human_segmentation.py
+++ human_segmentation.py
@@ -4,6 +4,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import torchvision
 import torchvision.models.detection as detec
+import numpy as np  # NumPy를 임포트합니다
 
 # 모델 불러오기
 model = detec.maskrcnn_resnet50_fpn_v2(weights=detec.MaskRCNN_ResNet50_FPN_V2_Weights.COCO_V1)
@@ -26,14 +27,41 @@ def segment_people(image_path):
     with torch.no_grad():
         prediction = model([img])  # 이미 device 설정됨
 
-    img = T.ToPILImage()(img.cpu().squeeze(0))
-    plt.imshow(img)
+    img_np = np.array(T.ToPILImage()(img.cpu().squeeze(0)))
     
-    person_masks = [prediction[0]['masks'][i, 0].cpu() for i, label in enumerate(prediction[0]['labels']) if label == 1]
+    person_masks = [prediction[0]['masks'][i, 0].cpu().numpy() for i, label in enumerate(prediction[0]['labels']) if label == 1]
     
     if person_masks:
-        all_masks = torch.stack(person_masks).sum(0) >= 1
-        plt.imshow(all_masks.cpu().numpy(), alpha=0.5, cmap='gray')
+        all_masks = np.stack(person_masks).sum(0) >= 1
+        masked_image = Image.fromarray((img_np * all_masks[..., None]).astype('uint8'))
+        masked_image.save('/home/kimyirum/Immersive-Experience-Design/human_test.png')  # 저장 경로 설정
+
+    plt.imshow(img_np)
+    if person_masks:
+        plt.imshow(all_masks, alpha=0.5, cmap='gray')
+    plt.axis('off')
+    plt.show()
+
+# 사람 한 명만 세그먼테이션 함수
+def segment_single_person(image_path):
+    img = load_image(image_path)
+    with torch.no_grad():
+        prediction = model([img])  # 이미 device 설정됨
+
+    img_np = np.array(T.ToPILImage()(img.cpu().squeeze(0)))
+    
+    person_masks = [(prediction[0]['masks'][i, 0].cpu().numpy(), np.sum(prediction[0]['masks'][i, 0].cpu().numpy())) 
+                    for i, label in enumerate(prediction[0]['labels']) if label == 1]
+    
+    if person_masks:
+        # 가장 큰 마스크 선택
+        largest_mask = max(person_masks, key=lambda x: x[1])[0]
+        masked_image = Image.fromarray((img_np * largest_mask[..., None]).astype('uint8'))
+        masked_image.save('/home/kimyirum/Immersive-Experience-Design/humansingle_test.png')  # 저장 경로 설정
+
+    plt.imshow(img_np)
+    if person_masks:
+        plt.imshow(largest_mask, alpha=0.5, cmap='gray')
     plt.axis('off')
     plt.show()
 
@@ -44,5 +72,7 @@ if __name__ == "__main__":
     model.to(device)
 
     # 이미지 경로 설정 및 함수 실행
-    image_path = '/home/kimyirum/Immersive-Experience-Design/human_test1.png'
-    segment_people(image_path)
+    name = 'human_0.png'
+    image_path = '/home/kimyirum/Immersive-Experience-Design/'+name
+    # segment_people(image_path)
+    segment_single_person(image_path)
